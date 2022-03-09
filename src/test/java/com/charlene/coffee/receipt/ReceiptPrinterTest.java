5898ff363e625cac56994db423bf6fab276703cb
David Durrant <david@durrant.se>
2022-03-09 09:13:17 +0100

    Minor readability update.

--- src/test/java/com/charlene/coffee/receipt/ReceiptPrinterTest.java
+++ src/test/java/com/charlene/coffee/receipt/ReceiptPrinterTest.java
@@ -3,11 +3,12 @@ package com.charlene.coffee.receipt;
 import com.charlene.coffee.saleitems.BaconRoll;
 import com.charlene.coffee.saleitems.Coffee;
 import com.charlene.coffee.saleitems.OrangeJuice;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.charlene.coffee.receipt.ReceiptPrinter.print;
 import static com.charlene.coffee.saleitems.Coffee.Extras.*;
 import static com.charlene.coffee.saleitems.Coffee.Size.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Verifies the creation of a receipt for Charlene's Coffee Corner
@@ -86,28 +87,28 @@ public class ReceiptPrinterTest {
     void shouldPrintEmptyReceipt() {
         var receipt = Receipt.fromItems();
 
-        Assertions.assertEquals(RECEIPT_FOR_NO_ITEMS, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_NO_ITEMS, print(receipt));
     }
 
     @Test
     void shouldPrintReceiptWithSingleCoffee() {
         var receipt = Receipt.fromItems(new Coffee(SMALL));
 
-        Assertions.assertEquals(RECEIPT_FOR_ONE_SMALL_COFFEE, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_ONE_SMALL_COFFEE, print(receipt));
     }
 
     @Test
     void shouldPrintReceiptWithTwoCoffees() {
         var receipt = Receipt.fromItems(new Coffee(SMALL), new Coffee(LARGE));
 
-        Assertions.assertEquals(RECEIPT_FOR_TWO_COFFEES, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_TWO_COFFEES, print(receipt));
     }
 
     @Test
     void shouldPrintReceiptWithCoffeeBaconRollAndOrangeJuice() {
         var receipt = Receipt.fromItems(new Coffee(MEDIUM), new BaconRoll(), new OrangeJuice());
 
-        Assertions.assertEquals(RECEIPT_FOR_ONE_MEDIUM_COFFEE_BACON_ROLL_AND_ORANGE_JUICE, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_ONE_MEDIUM_COFFEE_BACON_ROLL_AND_ORANGE_JUICE, print(receipt));
     }
 
     @Test
@@ -115,7 +116,7 @@ public class ReceiptPrinterTest {
         var receipt = Receipt.fromItems(new Coffee(MEDIUM, EXTRA_MILK, FOAMED_MILK,
                 SPECIAL_ROAST));
 
-        Assertions.assertEquals(RECEIPT_FOR_ONE_MEDIUM_COFFEE_WITH_EXTRAS, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_ONE_MEDIUM_COFFEE_WITH_EXTRAS, print(receipt));
     }
 
     @Test
@@ -127,7 +128,7 @@ public class ReceiptPrinterTest {
                 new BaconRoll()
         );
 
-        Assertions.assertEquals(RECEIPT_FOR_MEDIUM_COFFEE_WITH_EXTRA_AND_BACON_ROLLS, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_MEDIUM_COFFEE_WITH_EXTRA_AND_BACON_ROLLS, print(receipt));
     }
 
     @Test
@@ -146,6 +147,6 @@ public class ReceiptPrinterTest {
                 new OrangeJuice()
         );
 
-        Assertions.assertEquals(RECEIPT_FOR_BEVERAGE_DISCOUNT, ReceiptPrinter.print(receipt));
+        assertEquals(RECEIPT_FOR_BEVERAGE_DISCOUNT, print(receipt));
     }
 }
